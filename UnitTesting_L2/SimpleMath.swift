cdf2d97b929ebdee633d273dd4f86fff6ddb14c5
ZachMommaerts <zmommaerts@gmail.com>
2023-07-20 11:20:02 -0500

    - Create SimpleMath class - Create addTen and beSquare methods in SimpleMath class - Create setUp and tearDown functions for testing - Create functions to test addTen and beSquare methods

---
+++ UnitTesting_L2/SimpleMath.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleMath.swift
+//  UnitTesting_L2
+//
+//  Created by Zach Mommaerts on 7/20/23.
+//
+
+import Foundation
+
+class SimpleMath {
+    
+    func addTen(_ number: Int) -> Int {
+        return number + 10
+    }
+    
+    func beSquare(_ number: Int) -> Int {
+        return number * number
+    }
+    
+}
--- UnitTesting_L2Tests/UnitTesting_L2Tests.swift
+++ UnitTesting_L2Tests/UnitTesting_L2Tests.swift
@@ -9,28 +9,38 @@ import XCTest
 @testable import UnitTesting_L2
 
 final class UnitTesting_L2Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var simpleMath: SimpleMath!
+    
+    override func setUp() {
+        simpleMath = SimpleMath()
+    }
+    
+    override func tearDown() {
+        simpleMath = nil
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAddTenWorks() {
+        // Given - Arrange
+        var expected: Int = 0
+        
+        // When - Act
+        expected = simpleMath.addTen(10)
+        
+        // Then - Assert
+        XCTAssert(expected == 20, "This should be 20")
+        XCTAssertTrue(expected == 20)
+        XCTAssertEqual(expected, 20, "This should be 20")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testBeSquareSucceeds() {
+        // Given
+        var expected = 0
+        
+        // When
+        expected = simpleMath.beSquare(4)
+        
+        // Then
+        XCTAssertEqual(expected, 16)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
