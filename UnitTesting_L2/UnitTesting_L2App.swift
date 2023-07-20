81ca3bdf9b0be0045bb5ccadf4eaa68fa791d5a1
ZachMommaerts <zmommaerts@gmail.com>
2023-07-20 10:17:43 -0500

    Initial Commit

---
+++ UnitTesting_L2/UnitTesting_L2App.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTesting_L2App.swift
+//  UnitTesting_L2
+//
+//  Created by Zach Mommaerts on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitTesting_L2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
